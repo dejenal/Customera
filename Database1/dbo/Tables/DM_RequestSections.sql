81ea7515ae87f0fbe6beb535f64a8c2b3b780e71
dejen <dejenfasil@gmail.com>
2020-04-26 18:32:10 -0400

    new table added

---
+++ Database1/dbo/Tables/DM_RequestSections.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DM_RequestSections] (
+    [QuestionSetCode]       BIGINT         NOT NULL,
+    [TabID]                 BIGINT         NOT NULL,
+    [SectionName]           NVARCHAR (MAX) NOT NULL,
+    [SectionDisplayName]    NVARCHAR (MAX) NOT NULL,
+    [Order]                 DECIMAL (4, 2) NOT NULL,
+    [IsDeleted]             BIT            NOT NULL,
+    [Updated_Timestamp]     BINARY (8)     NOT NULL,
+    [ods_modified_datetime] DATETIME       NOT NULL,
+    CONSTRAINT [PK_DM_RequestSections] PRIMARY KEY CLUSTERED ([QuestionSetCode] ASC)
+);
+
